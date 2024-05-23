ab0e5df6a7394542a8ab4a6819d94cfe23af89cd
Arnould555 <melanyarnould@gmail.com>
2024-05-23 18:51:24 -0300

    Primer commit

---
+++ parcial/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+
+  return (
+    <>
+      <h1>Hola</h1>
+      <form></form>
+      <Card/>
+
+    </>
+  )
+}
+
+export default App
---
+++ parcial/src/components/Card.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>Soy una tarjeta</div>
+  )
+}
+
+export default Card
